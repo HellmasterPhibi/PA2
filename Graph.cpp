35ba76b9a74f398ffa7eb5f47d8e363e7dd927d7
HellmasterPhibi <hellpavlikova@gmail.com>
2016-06-07 22:09:27 +0200

    trying to make barPlot take file specifications

--- Graph.cpp
+++ Graph.cpp
@@ -8,9 +8,39 @@
 #include "Graph.h"
 
 
-Graph::Graph() {
+Graph::Graph(ifstream& infile) {
+     string line, tmp;
+     if (infile.is_open()){
+         getline (infile,line);
+        while ( getline (infile,line) ){
+            stringstream ss(line);
+            vector<string> tmpVec;
+            while(getline(ss, tmp,','))
+                tmpVec.push_back(tmp);
+            
+            TCathegory cath(tmpVec[0],stoi(tmpVec[3]));
+            cathegories.push_back(cath);
+        }
+         setRanks();
+        infile.close();
+    }
 }
 
+void Graph::setRanks(){
+    int max = 0;
+    for(auto i: cathegories){
+        if (max < i.count)
+            max = i.count;
+    }
+    cout << "max = " << max <<endl;
+    for(auto i: cathegories){
+        i.rank = (i.count * 10) / max;
+        cout << i.name << " ma rank " << i.rank << endl;
+    }
+    
+    
+    
+}
 Graph::Graph(const Graph& orig) {
 }
 
